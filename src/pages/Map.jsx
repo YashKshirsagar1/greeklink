ce3f9ea698cadadd6f6f9c45a59d2b0a9ac914f8
YashKshirsagar1 <127996689+YashKshirsagar1@users.noreply.github.com>
2026-03-23 23:13:01 -0400

    satellite map view

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -352,8 +352,8 @@ export default function Map() {
               style={{ height: '100%', width: '100%' }}
             >
               <TileLayer
-                url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-                attribution='&copy; <a href="https://carto.com/">CARTO</a>'
+                url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+                attribution='&copy; <a href="https://www.esri.com/">Esri</a>'
               />
 
               <MapEvents addingPin={addingPin} onMapClick={handleMapClick} />
