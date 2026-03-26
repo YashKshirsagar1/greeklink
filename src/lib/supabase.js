29a7d6f1f0233fa2223410d257c9041f35cdb8af
YashKshirsagar1 <127996689+YashKshirsagar1@users.noreply.github.com>
2026-03-26 00:20:12 -0400

    fix supabase connection

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://cxlgykkjpbytonkxmoir.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4bGd5a2tqcGJ5dG9ua3htb2lyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3MTAzNzgsImV4cCI6MjA4OTI4NjM3OH0.84J_V8hgy9gazfsNEGbZ2qOPUos-y_4QVZdWF7n39rc'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
